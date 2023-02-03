3bc959139606127cf0d613aa743e798e42944134
alexlumbreras <alex.lumlo@gmail.com>
2023-02-03 12:34:15 +0100

    Loading TopBar

--- src/sections/layout/Layout.tsx
+++ src/sections/layout/Layout.tsx
@@ -1,12 +1,23 @@
 import { Outlet } from "react-router";
 import { Link } from "react-router-dom";
+import TopBarProgress from "react-topbar-progress-indicator";
 
 import { ReactComponent as Brand } from "./brand.svg";
 import styles from "./Layout.module.scss";
+import TopBarProgressByLocation from "./TopBarProgressByLocation";
+
+TopBarProgress.config({
+	barColors: {
+		"0": "#fff",
+		"1.0": "#3cff64",
+	},
+	shadowBlur: 5,
+});
 
 export function Layout() {
 	return (
 		<>
+			<TopBarProgressByLocation />
 			<header className={styles.header}>
 				<Link to={"/"} style={{ textDecoration: "none" }}>
 					<section className={styles.header__container}>
