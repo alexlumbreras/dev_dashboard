75245447d07fdc451a3d1576ceeacda4711e3c7d
alexlumbreras <alex.lumlo@gmail.com>
2023-02-06 11:29:29 +0100

    LLAC

--- src/sections/gitHubRepositoryDetail/GitHubRepositoryDetailFactory.tsx
+++ src/sections/gitHubRepositoryDetail/GitHubRepositoryDetailFactory.tsx
@@ -1,11 +1,24 @@
+import React from "react";
+
 import { config } from "../../devdash_config";
+import { GitHubApiGitHubRepositoryPullRequestRepository } from "../../infrastructure/GitHubApiGitHubrepositoryPullRequestRepository";
 import { GitHubApiGitHubRepositoryRepository } from "../../infrastructure/GitHubApiGitHubRepositoryRepository";
 import { GitHubRepositoryDetail } from "./GitHubRepositoryDetail";
 
-const repository = new GitHubApiGitHubRepositoryRepository(config.github_access_token);
+const gitHubRepositoryRepository = new GitHubApiGitHubRepositoryRepository(
+	config.github_access_token
+);
+const gitHubRepositoryPullRequestRepository = new GitHubApiGitHubRepositoryPullRequestRepository(
+	config.github_access_token
+);
 
 export class GitHubRepositoryDetailFactory {
-	static create() {
-		return <GitHubRepositoryDetail repository={repository} />;
+	static create(): React.ReactElement {
+		return (
+			<GitHubRepositoryDetail
+				gitHubRepositoryRepository={gitHubRepositoryRepository}
+				gitHubRepositoryPullRequestRepository={gitHubRepositoryPullRequestRepository}
+			/>
+		);
 	}
 }
---
+++ src/sections/gitHubRepositoryDetail/useGitHubRepositoryPullRequests.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+import { RepositoryId } from "../../domain/GitHubRepository";
+import { GitHubRepositoryPullRequest } from "../../domain/GitHubRepositoryPullRequest";
+import { GitHubRepositoryPullRequestRepository } from "../../domain/GitHubRepositoryPullRequestRepository";
+
+export function useGitHubRepositoryPullRequests(
+	repository: GitHubRepositoryPullRequestRepository,
+	repositoryId: RepositoryId
+): { isLoading: boolean; pullRequests: GitHubRepositoryPullRequest[] } {
+	const [pullRequests, setPullRequests] = useState<GitHubRepositoryPullRequest[]>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+
+	useEffect(() => {
+		setIsLoading(true);
+		repository
+			.search(repositoryId)
+			.then((pullRequests) => {
+				setPullRequests(pullRequests);
+				setIsLoading(false);
+			})
+			.catch((err) => alert(err));
+	}, [repository, repositoryId]);
+
+	return {
+		pullRequests,
+		isLoading,
+	};
+}
