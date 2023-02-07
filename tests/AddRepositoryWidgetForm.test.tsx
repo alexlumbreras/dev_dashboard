a0c28c3a2a9406848fb21483c85d7ed55188c771
alexlumbreras <alex.lumlo@gmail.com>
2023-02-07 16:01:01 +0100

    Test the Widget Form

--- tests/AddRepositoryWidgetForm.test.tsx
+++ tests/AddRepositoryWidgetForm.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { mock } from "jest-mock-extended";
 import { RepositoryWidgetRepository } from "../src/domain/RepositoryWidgetRepository";
 import { AddRepositoryWidgetForm } from "../src/sections/dashboard/repositoryWidget/AddRepositoryWidgetForm";
+import userEvent from "@testing-library/user-event";
 
 describe("AddWidgetForm", () => {
 	const mockrepository = mock<RepositoryWidgetRepository>();
@@ -9,6 +10,24 @@ describe("AddWidgetForm", () => {
 	it("show widget form when add button is clicked", () => {
 		render(<AddRepositoryWidgetForm repository={mockrepository} />);
 
-		const button = screen.getByRole("button");
+		const button = screen.getByRole("button", {
+			name: new RegExp("Añadir repositorio", "i"),
+		});
+
+		userEvent.click(button);
+
+		const url = screen.getByLabelText(new RegExp("Url del repositorio", "i"));
+
+		expect(url).toBeInTheDocument();
+	});
+
+	it("save new widget when form is submitted", () => {
+		render(<AddRepositoryWidgetForm repository={mockrepository} />);
+
+		const button = screen.getByRole("button", {
+			name: new RegExp("Añadir repositorio", "i"),
+		});
+
+		userEvent.click(button);
 	});
 });
